9fdb92c60fe7ec65cf5bdbb2e4baccac2d07505a
invpe <106522950+invpe@users.noreply.github.com>
2023-11-02 21:30:23 +0100

    Update viewer.cpp

--- Viewer/viewer.cpp
+++ Viewer/viewer.cpp
@@ -6,27 +6,64 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 #include <vector>
 #include <string>
 #include <sstream> 
 
 #define WIDTH 640
 #define HEIGHT 480
- 
+bool bBreak = false;
+bool bAnimate = false;
 double rX=0; 
 double rY=0;
-double rZ = 0;
+double rZ = -6.0;
 struct tPosition
 {
     double x;
     double y;
     double z;
-};
-int iSimulationCounter = 0;
+};  
+std::string strFilename;
+int iSimulationSwitch = 0;
 int iSimulationFrame = 0;
 std::vector<double> vValues;
 std::vector<tPosition> vPositions; 
- 
+void captureScreenshot(const char* filename) {
+    int viewport[4];
+    glGetIntegerv(GL_VIEWPORT, viewport); // Get the current viewport dimensions
+
+    int width = viewport[2]; // Get the width of the viewport
+    int height = viewport[3]; // Get the height of the viewport
+
+    unsigned char* pixels = new unsigned char[width * height * 3];
+
+    glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+
+    std::ofstream screenshotFile(filename, std::ios::out | std::ios::binary);
+
+    if (!screenshotFile.is_open()) {
+        std::cerr << "Failed to open the file for writing." << std::endl;
+        delete[] pixels;
+        return;
+    }
+
+    screenshotFile << "P6\n" << width << " " << height << "\n255\n";
+
+    // Write the pixel data in reverse order to account for the upside-down orientation
+    for (int row = height - 1; row >= 0; row--) {
+        for (int col = 0; col < width; col++) {
+            int index = (row * width + col) * 3;
+            screenshotFile.put(pixels[index + 0]);
+            screenshotFile.put(pixels[index + 1]);
+            screenshotFile.put(pixels[index + 2]);
+        }
+    }
+
+    screenshotFile.close();
+    delete[] pixels;
+    std::cout << "Screenshot saved to " << filename << std::endl;
+}
 void renderText(float x, float y, const std::string& text) {
     glRasterPos2f(x, y);  
     for (char c : text) {
@@ -37,7 +74,6 @@ void reshape(int width, int height)
 { 
     glViewport(0, 0, width,height); 
 }
-
 void renderFunction ()
 { 
     glClearColor(0.0, 0.0, 0.0, 1.0);
@@ -48,12 +84,11 @@ void renderFunction ()
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity(); 
     glColor3f(1.0, 1.0, 1.0); 
-    renderText(-1,0.95,"[s]imulation: "+std::to_string(iSimulationCounter)+"/"+std::to_string(iSimulationFrame)+" points: "+std::to_string(vPositions.size()));
+    renderText(-1,0.95,"[s]imulation: "+std::to_string(iSimulationSwitch)+" points: "+std::to_string(vPositions.size())+" rx: "+std::to_string(rX)+" ry: "+std::to_string(rY)+" rz: "+std::to_string(rZ));
     gluPerspective(45.0, (float)WIDTH / (float)HEIGHT, 0.001, 100.0);
     gluLookAt(0,0,1, 0,0,0, 0,1,0);  
     
-
-     
+ 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity(); 
     glTranslatef(0.0f, 0.0f,  rZ);
@@ -61,15 +96,23 @@ void renderFunction ()
     glRotatef( rY, 0.0, 1.0, 0.0 ); 
 
     //  
-    glBegin(GL_POINTS); 
-    glColor4f(1,1,1,0.5);
- 
     for(int x = 0; x < iSimulationFrame;  x++)
-        glVertex3f(vPositions[x].x, vPositions[x].y, vPositions[x].z);
-               
-    glEnd();
+    {
+        if(x==iSimulationFrame-1)   
+            glColor4f(1,1,1,1);    
+        else 
+            glColor4f(1,1,1,0.25); 
+
+
+        glBegin(GL_POINTS); 
+        glVertex3f(vPositions[x].x, vPositions[x].y, vPositions[x].z); 
+        glEnd();
+    }
+
+ 
+    /* AXIS
+    glPointSize(2.0f);  
     glBegin(GL_POINTS);
-    
     for(int x=0; x < 10; x++)
     {
         glColor4f(1,0,0,1);
@@ -86,26 +129,33 @@ void renderFunction ()
 
     }
     glEnd();
- 
+ */
     
     glutSwapBuffers();  
 } 
-void timer(int value) { 
-
-
-    if(iSimulationCounter == 1)
+void Tick()
+{ 
+    if(iSimulationSwitch == 1)
     {
         iSimulationFrame++;
         if(iSimulationFrame>vPositions.size())
         iSimulationFrame=0; 
     }
+
+    if(bAnimate)
+    {
+        rX+=0.1;
+        rY+=0.1; 
+    }
   
+}
+void timer(int value) { 
+    Tick();
     glutPostRedisplay(); 
     glutTimerFunc(16, timer, 0);
 }
 void mouse(int button, int state, int x, int y) {
-	if(state == GLUT_DOWN && button == GLUT_LEFT_BUTTON) printf("LMB\n");
-       glutPostRedisplay();
+	if(state == GLUT_DOWN && button == GLUT_LEFT_BUTTON) printf("LMB\n"); 
 }
 void keyboard (int key, int x, int y)
 {
@@ -128,27 +178,52 @@ if (key == GLUT_KEY_RIGHT)
         }
     else if (key == GLUT_KEY_PAGE_UP)
         {
-                rZ += 0.1;
+                rZ += 0.05;
         }
     else if (key == GLUT_KEY_PAGE_DOWN)
         {
-                rZ -= 0.1;
+                rZ -= 0.05;
         } 
+}
+void Capture()
+{
+
+            std::string strSS = strFilename+".ppm";
+        captureScreenshot(strSS.c_str());
 }
  void keyboard_normal (unsigned char key, int x, int y)
 {
+        if(key=='a')
+        {
+            bAnimate = !bAnimate;
+        }
         if (key == 's')
         {
-            if(iSimulationCounter == 0)
-                iSimulationCounter = 1,iSimulationFrame = 0;
-            else iSimulationCounter = 0,iSimulationFrame = vValues.size();
+            if(iSimulationSwitch == 0)
+                iSimulationSwitch = 1,iSimulationFrame = 0;
+            else iSimulationSwitch = 0,iSimulationFrame = vValues.size();
 
-            printf("Simulation: %i\n", iSimulationCounter); 
+            printf("Simulation: %i\n", iSimulationSwitch); 
         }
+         if (key == 'z'  ) {Capture();}
 }
+ 
 int main(int argc, char *argv[])
 { 
-    std::ifstream file("input.txt");  
+    if(argc<2)
+    {
+        printf("./viewer <filename>\n");
+        exit(0);
+    }
+
+    if(argc==3)
+        bBreak=true;
+
+    // Add automate option to render,take screenshot and exit
+
+
+    strFilename=argv[1];
+    std::ifstream file(strFilename);  
     if (!file.is_open()) {
         std::cout << "Failed to open the file." << std::endl;
         return 1;
@@ -186,13 +261,14 @@ int main(int argc, char *argv[])
         _Position.y = vValues[a-1] - vValues[a-2];
         _Position.z = vValues[a-2] - vValues[a-3];
 
-        // Normalize to <0,1>
-//        normalized_value = (x - a) / (b - a)
+        // Normalize to <0,1> 
         _Position.x = (_Position.x-iMinValue)/(iMaxValue-iMinValue);
         _Position.y = (_Position.y-iMinValue)/(iMaxValue-iMinValue);
         _Position.z = (_Position.z-iMinValue)/(iMaxValue-iMinValue);
         vPositions.push_back(_Position);  
     }
+ 
+    iSimulationFrame =vPositions.size()-1;
     printf("Done loading\n");
 
     glutInit (&argc, argv);    
@@ -203,17 +279,17 @@ int main(int argc, char *argv[])
 
 
     // Enable Z-buffer depth test
-    //glEnable(GL_DEPTH_TEST);
-
+    //glEnable(GL_DEPTH_TEST); 
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
-    glPointSize(1.0f);  
+    glPointSize(2.0f);   
+    glLineWidth(2.0);
     glutDisplayFunc (renderFunction);
     glutReshapeFunc(reshape);
     glutSpecialFunc(keyboard);
     glutKeyboardFunc (keyboard_normal);
     glutMouseFunc(mouse);
-    glutTimerFunc(0, timer, 0);
+    glutTimerFunc(0, timer, 0); 
     glutMainLoop ();
 
     return 0;
